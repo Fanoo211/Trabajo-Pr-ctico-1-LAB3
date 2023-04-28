00825f37764018c8e5fbb8beca32e20fa190e794
Fanoo211 <ternavasiofano@gmail.com>
2023-04-28 19:51:47 -0300

    comentarios
    se modifico el agregado de comentarios

--- agregadoComentarios.js
+++ agregadoComentarios.js
@@ -9,11 +9,10 @@ function agregarComentario(evento) {
   
     const cajaComentarios = document.getElementById("caja-comentarios"); // seleccionar la caja de comentarios
     const nuevoComentario = document.createElement("p"); // crear un nuevo elemento <p>
-    nuevoComentario.textContent = `${nombre} ${comentario}`; // establecer el contenido del comentario con el nombre
+    nuevoComentario.innerHTML = `<span style="color: blue; font-size: 20px"><b>${nombre}</b></span> ${comentario}`; // establecer el contenido del comentario con el nombre
   
     cajaComentarios.appendChild(nuevoComentario); // agregar el nuevo comentario a la caja de comentarios
   
     nombreInput.value = ""; // borrar el contenido del campo de entrada del nombre
     comentarioInput.value = ""; // borrar el contenido del campo de entrada del comentario
-    emailInput.value = ""; // borrar el contenido del campo de entrada del correo
 }
