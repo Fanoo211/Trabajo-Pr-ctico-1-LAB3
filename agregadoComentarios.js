2adc27d7f38e37780282e831360c9b095419adf3
Fanoo211 <ternavasiofano@gmail.com>
2023-04-29 16:46:17 -0300

    Comentario - Boton Me gusta
    A los comentarios se le agrego mensaje de error y se le dio funcionalidad al botón me gusta

--- agregadoComentarios.js
+++ agregadoComentarios.js
@@ -1,17 +1,18 @@
 function agregarComentario(evento) {
     evento.preventDefault(); // prevenir el envío del formulario
   
-    const nombreInput = document.getElementById("nombre-usuario"); // seleccionar el campo de entrada del nombre
-    const nombre = nombreInput.value; // obtener el valor del nombre
+    let nombreInput = document.getElementById("nombre-usuario"); // seleccionar el campo de entrada del nombre
+    let nombre = nombreInput.value; // obtener el valor del nombre
   
-    const comentarioInput = document.getElementById("comentario"); // seleccionar el campo de entrada del comentario
-    const comentario = comentarioInput.value; // obtener el valor del comentario
+    let comentarioInput = document.getElementById("comentario"); // seleccionar el campo de entrada del comentario
+    let comentario = comentarioInput.value; // obtener el valor del comentario
   
-    const cajaComentarios = document.getElementById("caja-comentarios"); // seleccionar la caja de comentarios
-    const nuevoComentario = document.createElement("p"); // crear un nuevo elemento <p>
+    let cajaComentarios = document.getElementById("caja-comentarios"); // seleccionar la caja de comentarios
+    let nuevoComentario = document.createElement("p"); // crear un nuevo elemento <p>
 
     if(nombre == "" || comentario == ""){
-
+        comentario = "Error, falto ingresar nombre o comentario";
+        nuevoComentario.innerHTML = `<span style="color: red; font-size: 20px">${comentario}</span>`;
     }
     else{
         nuevoComentario.innerHTML = `<span style="color: blue; font-size: 20px"><b>${nombre}</b></span> ${comentario}`; // establecer el contenido del comentario con el nombre
---
+++ botonMeGusta.js
@@ -0,0 +1,30 @@
+// Obtener los elementos HTML del botón y el contador
+const botonMeGusta = document.getElementById("boton-me-gusta");
+const contadorMeGusta = document.getElementById("contador-me-gusta");
+
+// Inicializar el estado del botón y el contador
+let meGustaApretado = false;
+let contadorMeGustaValor = 200;
+
+// Agregar un evento clic al botón
+botonMeGusta.addEventListener("click", function() {
+  if (meGustaApretado) {
+    // Si ya está apretado, desapretarlo y restar 1 al contador
+    meGustaApretado = false;
+    contadorMeGustaValor--;
+  } else {
+    // Si no está apretado, apretarlo y sumar 1 al contador
+    meGustaApretado = true;
+    contadorMeGustaValor++;
+  }
+
+  // Actualizar la clase CSS del botón para reflejar el estado actual
+  if (meGustaApretado) {
+    botonMeGusta.classList.add("apretado");
+  } else {
+    botonMeGusta.classList.remove("apretado");
+  }
+
+  // Actualizar el texto del contador con el valor actual
+  contadorMeGusta.innerText = contadorMeGustaValor;
+});
