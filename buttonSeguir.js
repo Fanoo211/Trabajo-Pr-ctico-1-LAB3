2123752a89c3945b6d207224cddccbf9a9f20c94
samuel <129302031+samuelfrencia@users.noreply.github.com>
2023-04-30 19:13:00 -0300

    acomodado de boton seguir

--- buttonSeguir.js
+++ buttonSeguir.js
@@ -2,10 +2,12 @@ const seguirBtn = document.getElementById("seguirBtn");
 
 seguirBtn.addEventListener("click", function() {
   if (seguirBtn.innerHTML === "Seguir") {
-    seguirBtn.innerHTML = "Dejar de seguir";
+    seguirBtn.innerHTML = ' <span style="color: white;"> Dejar de seguir</span>' ;
+    seguirBtn.classList.add("apretado")
 
   } else {
     seguirBtn.innerHTML = "Seguir"; 
+    seguirBtn.classList.remove("apretado")
   }
-  
+
 });
