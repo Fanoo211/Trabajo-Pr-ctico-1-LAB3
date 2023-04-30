69a2370173ad59d755d300b4193274c5c43de8da
Fanoo211 <ternavasiofano@gmail.com>
2023-04-30 18:32:34 -0300

    Revert "Comentarios"
    This reverts commit ed36d94d167d025b9405b2736c74869658397cf8.

--- agregadoComentarios.js
+++ agregadoComentarios.js
@@ -1,33 +1,25 @@
 function agregarComentario(evento) {
-    evento.preventDefault(); 
+    evento.preventDefault(); // prevenir el envío del formulario
   
-    let nombreInput = document.getElementById("nombre-usuario"); 
-    let nombre = nombreInput.value.trim(); 
+    let nombreInput = document.getElementById("nombre-usuario"); // seleccionar el campo de entrada del nombre
+    let nombre = nombreInput.value; // obtener el valor del nombre
   
-    let comentarioInput = document.getElementById("comentario"); 
-    let comentario = comentarioInput.value.trim(); 
+    let comentarioInput = document.getElementById("comentario"); // seleccionar el campo de entrada del comentario
+    let comentario = comentarioInput.value; // obtener el valor del comentario
   
-    let cajaComentarios = document.getElementById("caja-comentarios"); 
-    let nuevoComentario = document.createElement("p"); 
+    let cajaComentarios = document.getElementById("caja-comentarios"); // seleccionar la caja de comentarios
+    let nuevoComentario = document.createElement("p"); // crear un nuevo elemento <p>
 
-    let mensajeError = ""; 
-  
-    if (nombre === "" || comentario === "") {
-        mensajeError = "Error, falta ingresar nombre o comentario"; 
-    } else {
-        nuevoComentario.innerHTML = `<span style="color: blue; font-size: 20px"><b>${nombre}</b></span> ${comentario}`; 
+    if(nombre == "" || comentario == ""){
+        comentario = "Error, falto ingresar nombre o comentario";
+        nuevoComentario.innerHTML = `<span style="color: red; font-size: 20px">${comentario}</span>`;
     }
+    else{
+        nuevoComentario.innerHTML = `<span style="color: blue; font-size: 20px"><b>${nombre}</b></span> ${comentario}`; // establecer el contenido del comentario con el nombre
 
-    if (mensajeError !== "") {
-        nuevoComentario.innerHTML = `<span style="color: red; font-size: 20px">${mensajeError}</span>`; 
-    } else {
-        while (cajaComentarios.firstChild) {
-            cajaComentarios.removeChild(cajaComentarios.firstChild);
-        }
-    }
-
-    cajaComentarios.appendChild(nuevoComentario); 
+    }  
+    cajaComentarios.appendChild(nuevoComentario); // agregar el nuevo comentario a la caja de comentarios
   
-    nombreInput.value = ""; 
-    comentarioInput.value = ""; 
+    nombreInput.value = ""; // borrar el contenido del campo de entrada del nombre
+    comentarioInput.value = ""; // borrar el contenido del campo de entrada del comentario
 }
